ef9deaef6709dd009da48bf179dcd562c3fadd6b
Guilherme Souza <ogrsouza@gmail.com>
2017-07-18 19:51:38 -0300

    Bucket struct defined

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+#define MAX_SIZE 12
+
+typedef struct {
+    int next;
+    int count;
+    int keys[MAX_SIZE];
+    int id;
+} Bucket;
+
 int main(int argc, const char *argv[]) {
     printf("Hello World\n");
     return 0;
