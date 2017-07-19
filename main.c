9826cc877569bcf5d31d559e212dd65273fef753
Guilherme Souza <ogrsouza@gmail.com>
2017-07-19 13:25:10 -0300

    Estruturas definidas e primeira tentativa de implementação da função make_address

--- main.c
+++ main.c
@@ -2,14 +2,40 @@
 
 #define MAX_SIZE 12
 
+int hash(int);
+
 typedef struct {
-    int next;
-    int count;
-    int keys[MAX_SIZE];
+    int prof;
+    int cont;
+    int chaves[MAX_SIZE];
     int id;
-} Bucket;
+} BUCKET;
+
+typedef struct {
+    BUCKET *bucket_ref;
+} DIR_CELL;
+
+typedef struct {
+    int prof;
+    DIR_CELL *celulas;
+} DIRETORIO;
+
+int make_address(int key, int prof) {
+    int retval = 0;
+    int mask = 1;
+    int hashval = hash(key);
+    
+    for (int j = 0; j < prof; j++) {
+        retval = retval << 1;
+        int lowbit = hashval & mask;
+        retval = retval | lowbit;
+        hashval = hashval > 1;
+    }
+
+    return retval;
+}
 
 int main(int argc, const char *argv[]) {
-    printf("Hello World\n");
+    
     return 0;
 }
