c24a2e254c4256c3228a17febf7076ef95070f21
Guilherme Souza <ogrsouza@gmail.com>
2017-07-27 18:35:03 -0300

    Script alterado para não gerar números repetidos

--- generator.py
+++ generator.py
@@ -2,6 +2,10 @@ import random, sys
 
 x = int(sys.argv[1])
 
+out = set([])
+
 for i in range(x):
-    r = random.randint(1, x)
-    print(r)
+    out.add(random.randint(1, x))
+
+for i in range(len(out)):
+    print(i)
